625f03d6dbda52838959f04e41355c1c6613d8cd
LucasQueiroz2 <lucasqueirozpl@gmail.com>
2025-02-20 21:27:00 -0300

    up

---
+++ untitled/src/controller/fase1.java
@@ -0,0 +1,90 @@
+package controller;
+ import java.util.Scanner;
+
+ import static java.lang.Math.sqrt;
+
+public class fase1 {
+    public static void main(String[] args) {
+
+
+//        10. Distância Entre Dois Pontos
+//        Desenvolva um programa que leia as coordenadas x1, y1 e x2, y2 de dois pontos no plano cartesiano e calcule a distância entre eles.
+//
+//        Fórmula: D = sqrt((x2x1)² + (y2 - y1)²)
+
+        Scanner sc = new Scanner(System.in);
+        //1
+//        System.out.print("digite um numero :");
+//        int num = sc.nextInt();
+//        System.out.print("digite outro numero :");
+//        int num2 = sc.nextInt();
+//        int res = num + num2;
+//        System.out.print(res);
+//       2
+//        System.out.print("coloque o raio de um circulo:");
+//        double raio = sc.nextDouble();
+//        double area = 3.149159 * (raio*raio);
+//        System.out.print("area do circulo: "+area);
+//       3
+//        System.out.println("Digite um temperatura em Fahrenheit: ");
+//        double celsius = sc.nextDouble();
+//
+//        double fahrenheit = (celsius * 9/5) + 32;
+//
+//        System.out.println("a temperatura em celsius:"+fahrenheit);
+        // 4
+//        System.out.print("digite a nota1:");
+//        double nota1 = sc.nextDouble();
+//        System.out.print("digite a nota2:");
+//        double nota2 = sc.nextDouble();
+//        System.out.print("digite a nota3:");
+//        double nota3 = sc.nextDouble();
+//        double media = (nota1+nota2+nota3)/3;
+//        5
+//        System.out.print("a media é " + media);
+//         System.out.print("digite sua idade");
+//         int idade = sc.nextInt();
+//         int dias = idade*365;
+//         System.out.print(dias);
+        //6
+//           System.out.print("digite seu salario bruto:");
+//           double salb = sc.nextDouble();
+//           System.out.print("digite o desconto do INSS:");
+//           double desconto = sc.nextDouble();
+//           double sall = salb-desconto;
+//             System.out.print("salario liquido"+sall);
+        //7
+//             System.out.print("digite a base de um retangulo:");
+//             int base = sc.nextInt();
+//             System.out.print("digite a altura de um retangulo:");
+//             int altura = sc.nextInt();
+//             int area=base*altura;
+//             int perimetro=(base+altura)*2;
+//             System.out.println("area do retangulo :"+area);
+//             System.out.println("perimetro:"+perimetro);
+        //8
+//               System.out.print("digite o raio de uma esfera:");
+//               double raio = sc.nextDouble();
+//               double volume = ((double) 4 /3)*3.14159*(raio*raio*raio);
+//               System.out.print("o volume é :"+volume);
+        //9
+//                System.out.print("digite um valor em reais");
+//                double valoreal = sc.nextDouble();
+//                System.out.print("digite o valor de 1 real pra dolar");
+//                double valoreua = sc.nextDouble();
+//                double cotacao = valoreal/valoreua;
+//                System.out.print("o valor em dolares é :"+cotacao);
+        //10
+//                  System.out.print("digite a coordenada x1:");
+//                  double x1 = sc.nextDouble();
+//                  System.out.print("digite a coordenada y1:");
+//                  double y1 = sc.nextDouble();
+//                  System.out.print("digite a coordenada x2:");
+//                  double x2 = sc.nextDouble();
+//                  System.out.print("digite a coordenada y2:");
+//                  double y2 = sc.nextDouble();
+//                  double D = sqrt((x2-x1)*(x2-x1) + (y2 - y1)*(y2 - y1));
+//                  System.out.print("a distancia entre os pontos é :"+D);
+
+    }
+}
